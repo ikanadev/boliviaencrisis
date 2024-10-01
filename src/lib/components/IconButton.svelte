8ace257250709900e50bdfe8bbac89d7be7b04ac
Kevin Vargas <vargaskevv@gmail.com>
2024-10-01 16:32:30 -0400

    IconButton component

---
+++ src/lib/components/IconButton.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import { type AnyMeltElement, emptyMeltElement, melt } from "@melt-ui/svelte";
+
+	export let onClick: VoidFunction;
+	export let type: "button" | "submit" | "reset" = "button";
+	export let size: "sm" | "md" | "lg" = "md";
+	export let element: AnyMeltElement = emptyMeltElement as AnyMeltElement;
+</script>
+
+<button
+	on:click={onClick}
+	{type}
+	use:melt={$element}
+	class="button"
+	class:button--small={size === "sm"}
+	class:button--medium={size === "md"}
+	class:button--large={size === "lg"}
+>
+	<slot />
+</button>
+
+<style lang="scss">
+	.button {
+		background: var(--bg, var(--surface-1));
+		color: var(--color, var(--text-2));
+		display: grid;
+		place-items: center;
+		border-radius: 50%;
+		filter: brightness(95%);
+		transition:
+			filter 0.2s ease-in-out,
+			transform 0.2s ease-in-out;
+		font-weight: 600;
+		&:hover {
+			filter: brightness(105%);
+		}
+		&--small {
+			padding: $size-2;
+			font-size: $font-size-sm;
+		}
+		&--medium {
+			padding: $size-3;
+			font-size: $font-size-md;
+		}
+		&--large {
+			padding: $size-3;
+			font-size: $font-size-lg;
+		}
+	}
+</style>
